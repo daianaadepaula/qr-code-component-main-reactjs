5b28443e8918fad7c9a764e939a3b4ef3fc1024b
Daiana de Paula <daianaadepaula1@gmail.com>
2023-10-18 10:28:54 -0300

    style(config): add configuration style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+		colors: {
+      'cardColor': 'hsl(0, 0%, 100%)',
+      'bgColor': 'hsl(212, 45%, 89%)',
+      'paragraphColor': 'hsl(220, 15%, 55%)',
+      'titleColor': 'hsl(218, 44%, 22%)',
+    },
     extend: {},
   },
   plugins: [],
