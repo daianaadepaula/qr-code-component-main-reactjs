dabec0fb2e9e6bb832dfa1a3b29b1b40fbc2c531
Daiana de Paula <daianaadepaula1@gmail.com>
2023-10-19 11:01:30 -0300

    feat(app): create app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-blue-600">
-        Hello world!
-      </h1>
-    </>
+    <main className="flex items-center justify-center h-screen w-screen bg-bgColor">
+      <div className="flex flex-col items-center gap-y-6 justify-center bg-cardColor rounded-lg py-5 max-w-xs">
+        <img
+          src="../src/assets/images/image-qr-code.png"
+          alt="Imagem do QR Code"
+          className="w-11/12 rounded-lg"
+        />
+        <h1 className="font-bold text-center text-2xl text-titleColor w-11/12">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="font-normal text-center text-base text-paragraphColor w-5/6 pb-2">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </main>
   );
 }
 
